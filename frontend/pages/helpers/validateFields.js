9d4ceba294184b8a71eae61d76fe59d347998d41
Fcasanova92 <fernando.casanova1992@outlook.com>
2024-04-25 11:34:05 -0300

    cambios en la validacion del formulario

--- frontend/pages/helpers/validateFields.js
+++ frontend/pages/helpers/validateFields.js
@@ -28,4 +28,32 @@ export const validateField = ({target}) => {
     }
 }
 
+export const validateForm = (event) => {
+
+  event.preventDefault()
+
+  const inputs = document.querySelectorAll("input[type='text']")
+
+  const inputsArray = Array.from(inputs)
+
+  let dataInput = []
+
+  inputsArray.map((input)=>{
+
+    console.log(input.value.trim())
+
+    if (input.value.length === 0){
+
+      // ubicar un mensaje , tengo que recuperar la validacion, tal vez cambiando el id con un "ok"
+
+      // cuando esas dos condiciones se cumplan, entonces se hace la insercion de la data
+
+    }
+
+    dataInput.push(input.value)
+
+  })
+
+}
+
 
