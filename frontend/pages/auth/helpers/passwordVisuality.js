eb12656d470cb324b154d4be9df44f59eae4e5ae
Fcasanova92 <fernando.casanova1992@outlook.com>
2024-05-14 12:09:14 -0300

    update(): Ojo de coontraseña funcional

--- frontend/pages/auth/helpers/passwordVisuality.js
+++ frontend/pages/auth/helpers/passwordVisuality.js
@@ -2,16 +2,22 @@ export const passwordVisuality = ({target}) =>{
 
     const eye = document.querySelector(`#${target.id}`);
 
+    console.log(eye)
+
     const password = document.querySelector("#password")
+
+    console.log(password.type)
 
     if(password.type === "password"){
 
         password.type = "text"
 
-        eye.innerHTML='<span id ="eye" class="material-symbols-outlined">visibility</span>'
+        console.log(password.type)
 
+        eye.textContent = "visibility"
 
+    }else{
+        password.type = "password"
+        eye.innerHTML= "visibility_off"   
     }
-    password.type = "password"
-    eye.innerHTML='<span id="eye" class="material-symbols-outlined">visibility_off</span>'
 }
