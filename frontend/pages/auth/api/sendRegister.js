e1470ad0eee07afde6f7237d1c5e83da7daec455
Fercasanov <fernando.casanova1992@outlook.com>
2024-05-19 13:36:16 -0300

    fixed(): estilos de google, se agregaron iconos

--- frontend/pages/auth/api/sendRegister.js
+++ frontend/pages/auth/api/sendRegister.js
@@ -1,4 +1,4 @@
-
+import {almacenarDatosUsuario} from '../../../db/usersMethods.js'
 
 export const sendRegister = async (data) => {
 
@@ -14,6 +14,8 @@ export const sendRegister = async (data) => {
     })
 
     if (send.request.status >= 200 && send.request.status < 300){
+
+      almacenarDatosUsuario(data)
       
       return {status:true}
 
