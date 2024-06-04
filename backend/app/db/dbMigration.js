545c566c97018d34f1f96849cc15b6bdad27c1e8
Fcasanova92 <fernando.casanova1992@outlook.com>
2024-06-04 11:30:14 -0300

    migracion(): el nuevo esquema no tiene errores

--- backend/app/db/dbMigration.js
+++ backend/app/db/dbMigration.js
@@ -8,25 +8,6 @@ dbConnection()
 
 const db = getConnection()
 
-db.query(` CREATE DATABASE IF NOT EXISTS ${process.env.DB_DATABASE}` , (err) => {
-    if (err) {
-      console.error('Error al crear la base de datos:', err);
-      db.end()
-      return;
-    }
-    console.log('Base de datos creada exitosamente');
-  });
-
-db.query(`USE ${process.env.DB_DATABASE}`, (err) => {
-    if (err) {
-        console.error('Error selecting database:', err);
-        db.end()
-        return;
-    }
-    console.log(`Using database ${process.env.DB_DATABASE}`);
-
-});
-
   // Ejecutar la consulta para crear la tabla 'users'
 db.query(`CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
