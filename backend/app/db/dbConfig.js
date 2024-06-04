98305aa32af14631aff891d7f37d46d444303d01
Fcasanova92 <fernando.casanova1992@outlook.com>
2024-06-04 11:09:09 -0300

    database():cambios en la migracion de los datos

--- backend/app/db/dbConfig.js
+++ backend/app/db/dbConfig.js
@@ -5,6 +5,7 @@ config()
 export const dbConfig = {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password : process.env.DB_PASSWORD}
+        password : process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE}
 
 
--- backend/app/db/dbConnection.js
+++ backend/app/db/dbConnection.js
@@ -2,39 +2,59 @@ import {dbConfig} from './dbConfig.js'
 
 import mysql from 'mysql'
 
-let connection;
+let conecction;
 
 export const dbConnection = () => {
 
-    connection = mysql.createConnection(dbConfig);
-    
-    connection.connect((error)=>{
-    
-        if(error){
-    
-            throw Error('error al conectar la base de datos :' + error)
-        }
-    
-        console.log('Servidor de base de datos correctamente conectado')
-    
-    })
+    const {host, user, password, database} = dbConfig
 
-    connection.on('error', (err) => {
-        console.error('Error en la conexión a la base de datos:', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            connectToDatabase(); // Reconecta automáticamente si se pierde la conexión
-        } else {
-            throw err;
-        }
-    })
+    try {
+
+        conecction = mysql.createConnection({
+            host: host,
+            user: user,
+            password: password
+        });
+        
+        conecction.connect((error)=>{
+        
+            if(error){
+        
+                throw Error('error al conectar la base de datos :' + error)
+            }
+        
+            console.log('Servidor de base de datos correctamente conectado')
+        
+        })
+    
+        conecction.query(`CREATE DATABASE IF NOT EXISTS \`${database}\``, (error) => {
+            if (error) {
+                throw new Error('Error al crear la base de datos: ' + error);
+            }
+            console.log(`Base de datos '${database}' creada o ya existente`)})
+
+    
+         conecction.changeUser({ database }, (error) => {
+            if (error) {
+                throw new Error('Error al conectar a la base de datos: ' + error);
+            }
+            console.log(`Conexión exitosa a la base de datos: ${database}`);
+        });
+        
+    } catch (error) {
+
+        console.error('Error al conectar a la base de datos:', error);
+        throw error;
+        
+    }
 
     }
 
 export const getConnection = () => {
-        if (!connection) {
+        if (!conecction) {
             throw new Error('No database connection available');
         }
-        return connection;
+        return conecction;
     };
 
 
