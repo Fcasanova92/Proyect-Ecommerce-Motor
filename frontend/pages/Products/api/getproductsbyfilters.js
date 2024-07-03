b63f4da086794943391e62bccec1b5335b852e95
Santiago Armendariz <santiarm91@gmail.com>
2024-07-02 21:14:54 -0300

    <<--Santi-->> *-Test getbyfilter

--- frontend/pages/Products/api/getproductsbyfilters.js
+++ frontend/pages/Products/api/getproductsbyfilters.js
@@ -1,29 +1,28 @@
 export const getproductsbyfilter = async (form) => {
     const data = filterValues(form);
-    console.log(data);
-    // try{
+    try{
 
-    //     const response =  await axios.post("http://127.0.0.1:3000/api/product/get-product-filter", data, {
+        const response =  await axios.post("http://127.0.0.1:3000/api/product/get-product-filter", data, {
 
-    //         'Content-Type': 'application/json'
-    //     })
+            'Content-Type': 'application/json'
+        })
 
-    //     const {token} = response.data
+        const {token} = response.data
 
-    //     if (response.status >= 200 && response.status < 300){
+        if (response.status >= 200 && response.status < 300){
         
-    //         sessionStorage.setItem('token', token);
+            sessionStorage.setItem('token', token);
                 
-    //         return {status:true, message:"Bienvenido a MotorShop"}
-    //     }
+            return {status:true, message:"Bienvenido a MotorShop"}
+        }
       
-    //     } catch (error) {
+        } catch (error) {
         
-    //     if(error.response.status === 401){
+        if(error.response.status === 401){
 
-    //         return {status:false, id:error.response.data.id , message:error.response.data.message}
-    //     }
-    // }
+            return {status:false, id:error.response.data.id , message:error.response.data.message}
+        }
+    }
 }
 
 const filterValues = (form) => {
