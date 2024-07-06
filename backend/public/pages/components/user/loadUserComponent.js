085a6d6c1f22e4200cae139749d880e933d2341c
Fercasanov <fernando.casanova1992@outlook.com>
2024-07-05 21:11:46 -0300

    ultimos arreglos de las rutas

--- backend/public/pages/components/user/loadUserComponent.js
+++ backend/public/pages/components/user/loadUserComponent.js
@@ -1,14 +1,13 @@
-
+import {loginComponent} from './componentesUsers.js'
 
 export const loadUserComponent = async (user) => {
     try {
-        const response = await fetch('/frontend/pages/components/user/loginComponent.html');
-        if (!response.ok) throw new Error('Error al cargar el componente HTML');
-
+ 
         const newNode = document.createElement('li')
         newNode.classList = "sub-menu"
 
-        newNode.innerHTML = await response.text();
+        newNode.innerHTML = loginComponent
+ 
         user.parentNode.replaceChild(newNode, user);
     } catch (error) {
         console.error('Error al cargar el componente HTML:', error);
