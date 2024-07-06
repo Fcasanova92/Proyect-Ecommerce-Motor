ca0bcc66bbf7d0b5c2b3528b733762ea747f37c7
Santiago Armendariz <santiarm91@gmail.com>
2024-07-06 11:46:44 -0300

    <<--Santi--> *-Cambios en rutas generales *-Anexo de los archivos userProfile y getDataProfile *-Anexo de estilos

---
+++ backend/public/pages/getDataProfile.js
@@ -0,0 +1,24 @@
+import {addNode} from './helpers/product/utilities/nodes.js';
+
+const getUserData = () => {
+    return new Promise(async (res, rej)=>{
+        const token = sessionStorage.getItem("token");
+        const resp = await axios.get("http://127.0.0.1:3000/api/auth/protected", {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return resp.status === 200 ? res(resp.data) : rej({});
+    });
+}
+
+const display = async () => {
+    const data = await getUserData(); 
+    if(data) {
+        const parent = document.getElementById('user-data');
+        for (const key in data) {
+            addNode(parent,'h2',{class:'title-c'}).textContent = key;
+            addNode(parent,'p',{class:'body-c'}).textContent = data[key];
+        }
+    }
+}
