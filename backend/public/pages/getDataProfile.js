cb4cbf34e5e7cf556fd4b92bcd47b9b22ab8395e
Santiago Armendariz <santiarm91@gmail.com>
2024-07-06 13:51:45 -0300

    <<--Santi-->> *-Cambios hasta donde pude

--- backend/public/pages/getDataProfile.js
+++ backend/public/pages/getDataProfile.js
@@ -1,24 +1,57 @@
 import {addNode} from './helpers/product/utilities/nodes.js';
 
-const getData = () => {
+const getUserData = () => {
     return new Promise(async (res, rej)=>{
-        const token = sessionStorage.getItem("token");
-        const resp = await axios.get("http://127.0.0.1:3000/api/auth/protected", {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-        return resp.status === 200 ? res(resp.data) : rej({});
+        try {
+            const token = sessionStorage.getItem("token");
+            const resp = await axios.get("http://127.0.0.1:3000/api/auth/protected", {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            return resp.status === 200 ? res(resp.data) : rej({});
+        } catch (error) {
+            console.log(error);
+        }
     });
 }
 
+const passUpdate = async (data) => {
+    return new Promise(async (res, rej)=>{
+        try {
+        // Aca la logica para cambiar el pass
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
+
+const handleUpdate = async (event) => {
+    event.preventDefault();
+    const data = Object.fromEntries(new FormData(event.target));
+    const resp = passUpdate(data);
+}
+
 const display = async () => {
-    const data = await getData(); 
-    if(data) {
-        const parent = document.getElementById('user-data');
-        for (const key in data) {
-            addNode(parent,'h2',{class:'title-c'}).textContent = key;
-            addNode(parent,'p',{class:'body-c'}).textContent = data[key];
+    //Usuario de ejemplo por q no puedo traer desde el Server
+    const user = {
+        id : 78,
+        nombre: 'jhon',
+        apellido: 'doe',
+        correo: 'jhondoe@example.com'
+    }; 
+    if(user) {
+        const user_data_attach = document.getElementById('user-data');
+        for (const key in user) {
+            if(key !== 'id') {
+                const title =  key.toString();
+                const body = user[key].toString();
+                addNode(user_data_attach,'h2',{class:'title-d'}).textContent = title[0].toUpperCase() + title.slice(1) + ':';
+                addNode(user_data_attach,'p',{class:'body-c'}).textContent = body + '.';
+            }
         }
     }
 }
+
+const run = display();
+document.getElementById('update-pass').addEventListener('submit',handleUpdate);
