3fb3ff17ec6a0ea1575660cd458c0984a8cd86a0
Santiago Armendariz <santiarm91@gmail.com>
2024-07-07 17:06:20 -0300

    <<--Santi-->>

--- backend/public/pages/Products/api/getProducts.js
+++ backend/public/pages/Products/api/getProducts.js
@@ -1,9 +1,7 @@
-import { BASE_URL } from "../../baseRoot";
-
 export const getProducts = () => {
     return new Promise(async (res,rej)=>{
         try {
-            const resp = await axios.get(`${BASE_URL}/api/product/get-product`);
+            const resp = await axios.get("https://proyect-ecommerce-motor-nnl6.onrender.com/api/product/get-product");
             if(resp){
                 return res(resp.data);
             }
@@ -17,7 +15,7 @@ export const getProducts = () => {
 export const getNewness = () => {
     return new Promise(async (res,rej)=>{
         try {
-            const resp = await axios.get(`${BASE_URL}/api/product/get-newness`);
+            const resp = await axios.get("https://proyect-ecommerce-motor-nnl6.onrender.com/api/product/get-newness");
             if(resp){
                 return res(resp.data);
             }
